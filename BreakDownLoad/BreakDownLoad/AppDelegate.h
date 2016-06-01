36c8ea2a8308e7b60c426d66a7b923ab4294a325
TongJC <student@tager.local>
2016-06-01 10:46:31 +0800

    JongJC
    下载

---
+++ BreakDownLoad/BreakDownLoad/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BreakDownLoad
+//
+//  Created by student on 16/3/17.
+//  Copyright © 2016年 Tom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BreakDownLoad/BreakDownLoad/ViewController.h
@@ -0,0 +1,17 @@
+//
+//  ViewController.h
+//  BreakDownLoad
+//
+//  Created by student on 16/3/17.
+//  Copyright © 2016年 Tom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+@property (retain, nonatomic) IBOutlet UIProgressView *progress;
+@property (retain, nonatomic) IBOutlet UILabel *labelText;
+
+
+@end
+
